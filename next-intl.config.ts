4e4e967e174af0df0df0eb372d3ec6992e8f7f30
Osman Yalçın <oygulmez97@gmail.com>
2025-09-29 16:39:53 +0300

    fix(i18n): add next-intl.config.ts and pass locale to getMessages to resolve runtime error

---
+++ next-intl.config.ts
@@ -0,0 +1,8 @@
+import {locales, defaultLocale} from './src/i18n';
+
+export default {
+  locales,
+  defaultLocale
+};
+
+
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -17,7 +17,7 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  const messages = await getMessages();
+  const messages = await getMessages({locale});
   return (
     <html lang={locale}>
       <body>
