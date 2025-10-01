ed3df96d9cee4bc7a7ddfcaf0cd3b50b26db35d9
Osman Yalçın <oygulmez97@gmail.com>
2025-10-01 12:43:59 +0300

    chore: remove FR pages; fix Cloudflare routing (_routes.json excludes static assets)

---
+++ .open-next/assets/_next/static/KOFztvrVUaoqSt85zwfoJ/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/ba9fafe44e39f73f.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
